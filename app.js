cb976bcc84aca9e129eab4554cc49d176f289330
jazzdragon <climbinghobo@gmail.com>
2017-01-11 19:40:28 -0700

    maybe?

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express')
+const path = require('path')
+const app = express()
 
-app.use(express.static('./build'));
+const httpProxy = require('http-proxy')
+const proxy = httpProxy.createProxyServer()
+
+app.use(express.static('./build'))
+
+app.all('/api/*', function (req, res) {
+    proxy.web(req, res, {
+        target: 'http://localhost:8080/api/'
+    })
+})
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, './build', 'index.html'));
-});
+  res.sendFile(path.join(__dirname, './build', 'index.html'))
+})
 
-app.listen(process.env.PORT || 9000);
+app.listen(process.env.PORT || 9000)
