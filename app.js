7f7e73e3093361f088cd839a37ab966073b002b5
jazzdragon <climbinghobo@gmail.com>
2017-01-12 11:35:48 -0700

    added comments

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+// App entry point, serves index.html statically on every url so we can use react router
+
 const express = require('express')
 const path = require('path')
 const app = express()
 
 const proxy = require('express-http-proxy')
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 3000
 
 app.use(express.static('./build'))
 
-
+// use this proxy to send any fetch to our back-end server
 app.use('/api', proxy(process.env.BACKEND_ADDR || 'http://localhost:3001'))
 
 app.get('/*', function (req, res) {
