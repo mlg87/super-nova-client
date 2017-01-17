10a53ae42c4f5ab7d6f784d053ffec580e8d9a60
jazzdragon <climbinghobo@gmail.com>
2017-01-17 14:38:00 -0700

    merge dev

--- src/index.js
+++ src/index.js
@@ -10,86 +10,9 @@ import 'override.css';
 
 const store = createStore(reducers)
 
-<<<<<<< HEAD
-ReactDOM.render(
-  <Router history={ browserHistory }>
-    <Route
-      path='/'
-      component={ AppLayout }
-    >
-      <IndexRoute
-        component={ LandingLayout }
-        isSubSideNavOpen={ false }
-      />
-      <Route
-        path='/register'
-        component={ Register }
-        isSubSideNavOpen={ false }
-      />
-      <Route
-        path='/inventory'
-        component={ InventoryLayout }
-        isSubSideNavOpen={ true }
-        subSideNavHeader='Inventory'
-        subSideNavLinks={ subSideNavLinks.inventory }
-      >
-        <Route
-          path='/inventory/add'
-          component={ NotFound }
-        />
-        <Route
-          path='/inventory/edit'
-          component={ NotFound }
-        />
-        <Route
-          path='/inventory/all'
-          component={ NotFound }
-        />
-        <Route
-          path='/inventory/settings'
-          component={ NotFound }
-        />
-      </Route>
-      <Route
-        path='/customers'
-        component={ UnderConstruction }
-        isSubSideNavOpen={ false }
-      />
-      <Route
-        path='/reservations'
-        component={ Reservations }
-        isSubSideNavOpen={ false }
-      />
-      <Route
-        path='/users'
-        component={ UsersLayout }
-        isSubSideNavOpen={ true }
-        subSideNavHeader='Users'
-        subSideNavLinks={ subSideNavLinks.users }
-      >
-        <Route
-          path='/users/add'
-          component={ Register }
-        />
-      </Route>
-      <Route
-        path='/current_user'
-        component={ CurrentUser }
-        isSubSideNavOpen={ false }
-      />
-      <Route
-        path='*'
-        component={ NotFound }
-        isSubSideNavOpen={ false }
-      />
-    </Route>
-  </Router>,
-  document.getElementById('root')
-=======
 render(
   <Provider store={store}>
     {AppRouter}
   </Provider>
   , document.getElementById('root')
->>>>>>> 73d193b96d554fc5e08e782e35c6ce3cda867831
 )
