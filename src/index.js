32cb3a2c4759a2c767de43641348205c5555a205
jazzdragon <climbinghobo@gmail.com>
2017-01-17 10:21:38 -0700

    threw it in there

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+import React from 'react'
+import { AppRouter } from 'bin/router'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { reducers } from 'reducers'
+
 // just some styles for the boys
 import 'override.css';
-import 'bin/router'
+
+
+const store = createStore(reducers)
+
+render(
+  <Provider store={store}>
+    {AppRouter}
+  </Provider>
+  , document.getElementById('root')
+)
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import { test } from 'reducers/test'
+
+export const reducers = combineReducers({
+  test
+})
