25b04fe98443ca645bc1f732ee861abed19af79e
jazzdragon <climbinghobo@gmail.com>
2017-01-20 16:18:42 -0700

    half way tests

--- src/components/inventorySelect/InventorySelect.spec.js
+++ src/components/inventorySelect/InventorySelect.spec.js
@@ -1,3 +1,39 @@
-it('true', () => {
-  expect(true).toBeTruthy()
+import React from 'react'
+import { shallow } from 'enzyme'
+import InventorySelect from './InventorySelect'
+
+describe('InventorySelect', () => {
+  const setup = () => {
+    return shallow(<InventorySelect />)
+  }
+
+  it('renders without crashing', () => {
+    setup()
+    expect(true).toBe(true)
+  })
+
+  it('contains a wrapper div with a flexbox grid', () => {
+    const wrapper = setup()
+    const div = wrapper.find('div')
+    expect(div.length).toBe(1)
+    expect(div.props().style).toEqual({
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around'
+    })
+  })
+
+  it('contains a GridList', () => {
+    const wrapper = setup()
+    const gridList = wrapper.find('GridList')
+    expect(gridList.length).toBe(1)
+    const gridListProps = gridList.props()
+    expect(gridListProps.cellHeight).toBe(180)
+    expect(gridListProps.style).toEqual({
+      width: 500,
+      height: 450,
+      overflowY: 'auto'
+    })
+
+  })
 })
