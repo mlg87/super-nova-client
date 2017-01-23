4fc13421c34a553a2aa1c62c03c82457756398e6
Mason <masonlgoetz@gmail.com>
2017-01-23 16:38:57 -0700

    set up react-router-redux

--- src/index.js
+++ src/index.js
@@ -2,27 +2,15 @@ import React from 'react'
 import { AppRouter } from 'bin/router'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
-// browser history link up
-import { syncHistoryWithStore } from 'react-router-redux'
-import { browserHistory } from 'react-router'
-import { reducers } from 'reducers'
-import thunk from 'redux-thunk'
+import { store } from 'bin/router'
 import DevTools from 'containers/DevTools'
+
 // NOTE: this will be re-implemented when we get the API
 // to work like danny's real-world example
 // import userApi from 'middleware/users'
 
 // just some styles for the boys
 import 'override.css';
-
-const store = createStore(
-  reducers,
-  compose(applyMiddleware(thunk),
-  DevTools.instrument())
-)
-
-export const history = syncHistoryWithStore(browserHistory, store)
 
 render(
   <Provider store={ store }>
