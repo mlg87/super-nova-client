355a345574ebc6f4aaa944092895b92d2bc2070f
jazzdragon <climbinghobo@gmail.com>
2017-01-24 07:55:29 -0700

    starting on Remove

--- src/components/selectedInventoryList/SelectedInventoryList.jsx
+++ src/components/selectedInventoryList/SelectedInventoryList.jsx
@@ -3,6 +3,7 @@ import {List, ListItem} from 'material-ui/List';
 import Subheader from 'material-ui/Subheader'
 import { connect } from 'react-redux'
 import Avatar from 'material-ui/Avatar'
+import RemoveItem from 'material-ui/svg-icons/content/clear'
 
 const listStyle = {
   position: 'absolute',
@@ -13,27 +14,33 @@ const mapStateToProps = (state) => ({
   items: state.reservationSelectedInventory
 })
 
-const renderItems = (items) => {
+const mapDispatchToProps = (dispatch) => ({
+  removeInventory: () => console.log('hi')
+})
+
+const renderItems = ({ items, removeInventory }) => {
   return items.map((item) => {
     return (
       <ListItem
         key={item.uuid}
         primaryText={item.model}
         leftAvatar={<Avatar src={item.image_url} />}
+
+        rightIcon={<RemoveItem onClick={removeInventory}/>}
       />
     )
   })
 }
 
-const SelectedInventoryList = ({ items }) => {
+const SelectedInventoryList = (props) => {
   return (
     <List style={listStyle}>
-      { items.length ? <Subheader>Selected Gear</Subheader> : ''}
-      { renderItems(items) }
+      { props.items.length ? <Subheader>Selected Gear</Subheader> : ''}
+      { renderItems(props) }
     </List>
   )
 }
 
 export default connect(
-  mapStateToProps, null
+  mapStateToProps, mapDispatchToProps
 )(SelectedInventoryList)
