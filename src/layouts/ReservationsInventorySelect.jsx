6ae2ebbb196b58d53d8715beb7b14461950f318e
jazzdragon <climbinghobo@gmail.com>
2017-01-23 18:46:34 -0700

    extract serach

--- src/layouts/ReservationsInventorySelect.jsx
+++ src/layouts/ReservationsInventorySelect.jsx
@@ -2,31 +2,14 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Center } from 'components/center/Center';
 import InventoryList from 'components/inventoryList/InventoryList'
-import TextField from 'material-ui/TextField'
-import Chip from 'material-ui/Chip'
-import { updateSearchTerms, fetchInventory } from 'actions/inventory'
+import InventorySearch from 'components/inventorySearch/InventorySearch'
+import { fetchInventory } from 'actions/inventory'
 import { Link } from 'react-router'
 import RaisedButton from 'material-ui/RaisedButton'
-
-const mapStateToProps = (state) => ({
-  searchTerms: state.inventorySearchTerms,
-})
 
 const mapDispatchToProps = (dispatch) => ({
   fetchInitialInventory: () => {
     dispatch(fetchInventory(''))
-  },
-
-  handleSearchChange: (e) => {
-    if (e.which === 13){
-      dispatch(updateSearchTerms('add', e.target.value))
-      // this does not bring back the placeholder text, tough...
-      e.target.value = ''
-    }
-  },
-
-  removeSearchTerm: (searchTerm) => {
-    dispatch(updateSearchTerms('remove', searchTerm))
   }
 })
 
@@ -36,32 +19,11 @@ class ReservationsInventorySelect extends Component {
     props.fetchInitialInventory()
   }
 
-  renderSearchTerms() {
-    return this.props.searchTerms.map((searchTerm) => {
-      return (
-        <Chip
-          key={searchTerm}
-          style={{margin: 4}}
-          onRequestDelete={() => this.props.removeSearchTerm(searchTerm)}
-        >
-          {searchTerm}
-        </Chip>
-      )
-    })
-  }
-
   render() {
     return (
       <Center>
         <div>
-          <div style={{display: 'flex'}}>
-            {this.renderSearchTerms()}
-          </div>
-          <TextField
-            name='inventory-search'
-            hintText='search for inventory'
-            onKeyUp={this.props.handleSearchChange}
-            />
+          <InventorySearch />
 
           <InventoryList />
 
@@ -97,5 +59,5 @@ class ReservationsInventorySelect extends Component {
 }
 
 export default connect(
-  mapStateToProps, mapDispatchToProps
+  null, mapDispatchToProps
 )(ReservationsInventorySelect)
